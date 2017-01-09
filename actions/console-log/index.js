3c7d7658190e88663a9f6930882496e0c7be4cdb
Michael Kelly <mkelly@mozilla.com>
2017-01-09 15:00:11 -0800

    Retire repo and point to monorepo.

--- actions/console-log/index.js
+++
@@ -1,9 +0,0 @@
-import {Action, registerAction} from '../utils';
-
-export default class ConsoleLogAction extends Action {
-    async execute() {
-        this.normandy.log(this.recipe.arguments.message, 'info');
-    }
-}
-
-registerAction('console-log', ConsoleLogAction);
